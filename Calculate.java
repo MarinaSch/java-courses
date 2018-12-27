ec8b3b14aa272e97d787538875940845e0a987ae
Marina <Marina_Schastna@ukr.net>
2018-12-27 02:01:24 +0200

    First Commit

---
+++ Calculate.java
@@ -0,0 +1,17 @@
+public class Calculate{
+
+	public static void main(String[]arg){
+	System.out.println("Calculate..");
+	
+	int a = 3;
+	int b = 6;
+	
+	int sum = a+b;
+	int minus = a-b;
+	
+	
+	System.out.println(sum +" "+ minus);
+	
+	}
+
+}
